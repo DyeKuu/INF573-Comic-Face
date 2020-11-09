8494d5354cd2e66251d66104b9e21c1f2f24ea3f
qym7 <876444960@qq.com>
2020-11-09 11:10:32 +0100

    update

--- demo.py
+++ demo.py
@@ -4,26 +4,14 @@ os.environ['CUDA_VISIBLE_DEVICES'] = "-1"
 
 from mtcnn import MTCNN
 import cv2
-from PIL import Image
-
-def collage(im1, im2):
-    # Read and turn picture into same size
-    width, height = im1.size
-    # Create a blank image
-    result = Image.new(im1.mode, (width * 2, height))
-    # Concat images
-    result.paste(im1, box=(0, 0))
-    result.paste(im2, box=(width, 0))
-
-    return result
 
 def compare_faces(filename1, filename2):
-    im1 = cv2.cvtColor(cv2.imread("img.PNG"), cv2.COLOR_BGR2RGB)
-    im2 = cv2.cvtColor(cv2.imread("img.PNG"), cv2.COLOR_BGR2RGB)
+    im1 = cv2.cvtColor(cv2.imread(filename1), cv2.COLOR_BGR2RGB)
+    im2 = cv2.cvtColor(cv2.imread(filename2), cv2.COLOR_BGR2RGB)
     dim = (1280, 1280)
     im1 = cv2.resize(im1, dim, interpolation=cv2.INTER_AREA)
     im2 = cv2.resize(im1, dim, interpolation=cv2.INTER_AREA)
-    full_image = collage(im1, im2)
+    full_image = cv2.hconcat([im1,im2])
 
     detector = MTCNN()
     res1 = detector.detect_faces(im1)[0]['keypoints']
@@ -36,6 +24,7 @@ def compare_faces(filename1, filename2):
 
     return full_image
 
-
+res = compare_faces("img.PNG", "ki.png")
+cv2.imshow("comparaison", res)
 
 
