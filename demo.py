d245d97c225e44d31fa1df855116fc3a05e3f63c
DyeKuu <kunhao.zheng@polytechnique.edu>
2020-11-09 14:32:28 +0100

    wip

--- demo.py
+++ demo.py
@@ -1,30 +1,59 @@
+import cv2 as cv
+from mtcnn import MTCNN
 import os
+import numpy as np
 os.environ['CUDA_DEVICE_ORDER'] = "PCI_BUS_ID"
 os.environ['CUDA_VISIBLE_DEVICES'] = "-1"
 
-from mtcnn import MTCNN
-import cv2
 
 def compare_faces(filename1, filename2):
-    im1 = cv2.cvtColor(cv2.imread(filename1), cv2.COLOR_BGR2RGB)
-    im2 = cv2.cvtColor(cv2.imread(filename2), cv2.COLOR_BGR2RGB)
-    dim = (1280, 1280)
-    im1 = cv2.resize(im1, dim, interpolation=cv2.INTER_AREA)
-    im2 = cv2.resize(im1, dim, interpolation=cv2.INTER_AREA)
-    full_image = cv2.hconcat([im1,im2])
+    im1 = cv.imread(filename1)
+    height, width, channels = im1.shape
 
+    im2 = cv.cvtColor(cv.imread(filename2), cv.COLOR_BGR2RGB)
+    im2 = cv.resize(im2, (width, height), interpolation=cv.INTER_AREA)
+    print(im2.shape)
     detector = MTCNN()
     res1 = detector.detect_faces(im1)[0]['keypoints']
     res2 = detector.detect_faces(im2)[0]['keypoints']
+    full_image = cv.hconcat([im1, im2])
 
     for k in res1:
-        cv2.line(full_image, res1[k], res2[k] + dim[0], (0, 255, 0), thickness=2)
+        cv.line(full_image, res1[k], (res2[k][0] +
+                                      width, res2[k][1]), (0, 255, 0), thickness=2)
 
-    cv2.imshow("Face comparaison", full_image)
-
-    return full_image
-
-res = compare_faces("img.PNG", "ki.png")
-cv2.imshow("comparaison", res)
+    # cv.imshow("Face comparaison", full_image)
+    # cv.waitKey(0)
+    # cv.destroyAllWindows()
+    return im1, cv.cvtColor(im2, cv.COLOR_BGR2RGB), res1, res2
 
 
+im1, im2, real_pts, comic_pts = compare_faces("img.PNG", "ki.png")
+height, width, channels = im1.shape
+print(np.array(list(real_pts.values())))
+# print(real_pts, comic_pts)
+
+M, mask = cv.findHomography(np.array(list(comic_pts.values())), np.array(
+    list(real_pts.values())), cv.RANSAC, 5.0)
+
+# cv.warpPerspective(im1, np.eye(3), (width, height))
+# cv.warpPerspective(	im2, M, ())
+
+dst = cv.warpPerspective(
+    im2, M, (im1.shape[1], im1.shape[0]))  # wraped image
+for i in range(height):
+    if (dst[i, ] == 0).all():
+        dst[i, ] = im1[i, ]
+        continue
+    for j in range(width):
+        # if not dst[i, j].all(0):
+        #     dst[i, j] = im1[i, j]
+        if (dst[i, j] == 0).all():
+            dst[i, j] = im1[i, j]
+        # print(temp)
+# now paste them together
+# dst[0:im1.shape[0], 0:im1.shape[1]] = im1
+cv.imshow("Face comparaison", dst)
+cv.waitKey(0)
+cv.destroyAllWindows()
+cv.imwrite("fusion.jpg", dst)
